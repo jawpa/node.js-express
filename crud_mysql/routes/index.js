65c790915fc3228f6b25e20de14fa96d23080e48
jawpa <jawpa2@hotmail.com>
2018-11-13 22:45:10 -0300

    módulo read y módulo create

--- crud_mysql/routes/index.js
+++ crud_mysql/routes/index.js
@@ -51,12 +51,50 @@ router
 	    			data : rows
 	    		}
 	    		// le pasamos datos a la vista en index.jade
-	        	res.render('index',locals);
+	        	res.render('index2',locals);
 			});
       	});
       	// no podemos ejecutar el siguiente middleware en el stack porque sería el error
       	//next()
 	})
+
+	// ruta agregar (del home al formulario)
+	.get('/agregar',(req, res, next) => {
+		// llamamos a una vista
+		// le pasamos un título
+		res.render('add-movie',{title: 'Agregar Película'});
+	})
+
+
+	// ruta del formmulario de agregar al home con la peli incorporada
+	.post('/',(req, res, next) => {
+		req.getConnection((err, connection) => {
+	    	if (err) return next(err);
+	      
+	    	// traemos la info del formulario, mediante el atributo name
+	    	let movie = {
+	    		// tomamos lo que viene del cuerpo del formulario
+	    		movie_id : req.body.movie_id,
+	    		title : req.body.title,
+	    		release_year : req.body.release_year,
+	    		rating : req.body.rating,
+	    		image : req.body.image
+	    	}
+
+	    	// probamos los datos recibidos en la consola
+	      	console.log(movie)
+
+	      	// reemplazamos al comodín ? por el contenido de la variable movie
+	    	connection.query('INSERT INTO movie set ? ', movie, (err, rows) => {
+	    		// si hay un error, que nos redirija al formulario
+	    		return (err) ? res.redirect('/agregar') : res.redirect('/')
+
+			});
+      	});
+      	
+	})
+
+
 	// no puedo usar el método get porque necesito una ruta
 	// le decimos que use el middleware
 	// tiene que estar declarado al final para que no marque error
